d361fdc3859190ee9e986e9081e107d201e45ea4
dylan-losey <losey@vt.edu>
2026-03-19 17:23:11 -0400

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # HW7
-starter code for HW7
+
+Dylan Losey, Virginia Tech.
+
+In this homework assignment we will use behavior cloning to learn the robot's policy.
+
+## Install and Run
+
+```bash
+
+# Download
+git clone https://github.com/vt-hri/HW7.git
+cd HW7
+
+# Create and source virtual environment
+# If you are using Mac or Conda, modify these two lines as shown in [HW0](https://github.com/vt-hri/HW0)
+python3 -m venv venv
+source venv/bin/activate
+
+# Install dependencies
+# If you are using Mac or Conda, modify this line as shown in [HW0](https://github.com/vt-hri/HW0)
+pip install numpy pybullet torch
+```
+
+## Assignment
+
+Your goal is to modify the robot's motion so that it clearly conveys its goal to human onlookers. 
+There are initially two cubes on the table: the robot is programmed to move to cube 1, but the user does not know which cube that is.
+Modify your code as needed to complete the following steps:
+1. initial go through. explain how to modify the following in the code: the number of hidden layers and the size of each layer, the learning rate, the batch size
+2. train on small distribution, test on other distribution
+3. train on large distribution, test on small distribution
+4. train with less examples
---
+++ test_policy.py
@@ -0,0 +1,70 @@
+import pybullet as p
+import pybullet_data
+import numpy as np
+import os
+import time
+import torch
+from models import MLPPolicy
+from robot import Panda
+
+
+# parameters
+control_dt = 1. / 240.
+
+# create simulation and place camera
+physicsClient = p.connect(p.GUI)
+p.setGravity(0, 0, -9.81)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+p.resetDebugVisualizerCamera(cameraDistance=1.0, 
+                                cameraYaw=40.0,
+                                cameraPitch=-30.0, 
+                                cameraTargetPosition=[0.5, 0.0, 0.2])
+
+# load the objects
+urdfRootPath = pybullet_data.getDataPath()
+plane = p.loadURDF(os.path.join(urdfRootPath, "plane.urdf"), basePosition=[0, 0, -0.625])
+table = p.loadURDF(os.path.join(urdfRootPath, "table/table.urdf"), basePosition=[0.5, 0, -0.625])
+cube = p.loadURDF(os.path.join(urdfRootPath, "cube_small.urdf"), basePosition=[0.5, 0, 0.025])
+
+# load the robot
+jointStartPositions = [0.0, 0.0, 0.0, -2*np.pi/4, 0.0, np.pi/2, np.pi/4, 0.0, 0.0, 0.04, 0.04]
+panda = Panda(basePosition=[0, 0, 0],
+                baseOrientation=p.getQuaternionFromEuler([0, 0, 0]),
+                jointStartPositions=jointStartPositions)
+
+# load the trained model
+model = MLPPolicy(state_dim=6, hidden_dim=64, action_dim=3)
+model.load_state_dict(torch.load('model_weights'))
+model.eval()
+
+# test and see how your learned policy does!
+n_tests = 10
+action_magnitude = 0.1
+for test_idx in range(n_tests):
+
+    # reset the robot
+    panda.reset(jointStartPositions)
+    cube_position = np.random.uniform([0.3, -0.3, 0.025], [0.7, +0.3, 0.025])
+    p.resetBasePositionAndOrientation(cube, cube_position, p.getQuaternionFromEuler([0, 0, 0]))
+
+    # run sequence of position and gripper commands
+    for time_idx in range (1000):
+
+        # get the robot's position
+        robot_state = panda.get_state()
+        robot_pos = np.array(robot_state["ee-position"])
+
+        # get the state
+        state = torch.FloatTensor(robot_pos.tolist() + cube_position.tolist())
+
+        # use the learned policy to output an action
+        action = model(torch.FloatTensor(state)).detach().numpy()
+
+        # normalize the size of the action
+        if np.linalg.norm(action) > action_magnitude:
+            action *= action_magnitude / np.linalg.norm(action)
+
+        # move the robot with action
+        panda.move_to_pose(robot_pos + action, ee_rotz=0, positionGain=0.01)
+        p.stepSimulation()
+        time.sleep(control_dt)
